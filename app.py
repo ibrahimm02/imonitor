4b7245d43c41523c8869a7ba062a9a63f75b8705
ibrahimm02 <ibm34427@gmail.com>
2022-12-23 20:33:36 +0000

    Added EC2 metrics

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from urllib import response
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, current_app
 from flask_bootstrap import Bootstrap
 import boto3
 from boto3 import Session
 from flask_cors import CORS
 from datetime import datetime, timedelta
+from operator import itemgetter
 import json
 import base64
 import io
@@ -12,6 +13,8 @@ import io
 ec2_client = boto3.client("ec2", region_name="us-east-1")
 
 ec2_resource = boto3.resource('ec2', region_name="us-east-1")
+
+cw_client = boto3.client('cloudwatch')
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -66,14 +69,17 @@ def get_running_ec2_instances():
 
     ec2_graph = get_ec2_graph()
 
+    ec2_metrics = ec2_metrics_amm()
+
     ec2_states = ec2_state_stats()
     # data = {'chart_data': ec2_states}
+    
 
     if not(instances):
         return render_template("aws/aws/ec2.html", message="No instance Data")
 
     return render_template("aws/aws_ec2.html", instances=instances, active_instances=active_instances, 
-            total_instances=total_instances, graphImage=ec2_graph, data=json.dumps(ec2_states))
+            total_instances=total_instances, graphImage=ec2_graph, ec2_metrics=ec2_metrics)
 
 @app.route('/aws/s3-instance-all')
 def s3_instances():
@@ -247,7 +253,7 @@ def get_active_instance_count():
     # print(f'Instances in state "{INSTANCE_STATE}":')
     count = 0
     for instance in instances:
-        print(f'  - Instance ID: {instance.id}')
+        # print(f'  - Instance ID: {instance.id}')
         count += 1
     
     return count
@@ -267,7 +273,7 @@ def instance_output_format(instance_data):
 
 def get_ec2_graph():
   
-    client = boto3.client('cloudwatch')
+    cw_client = boto3.client('cloudwatch')
 
     data=[]
     response_data=[]
@@ -282,7 +288,7 @@ def get_ec2_graph():
 
     # print(data)
 
-    response = client.get_metric_widget_image(MetricWidget=json.dumps(filter_data))
+    response = cw_client.get_metric_widget_image(MetricWidget=json.dumps(filter_data))
     bytes_data=io.BytesIO(response["MetricWidgetImage"])
     fr=base64.b64encode(bytes_data.getvalue())
     
@@ -316,6 +322,82 @@ def ec2_state_stats():
         graph_data.append({"key":i,"value":int(stats[i])})
     return graph_data
 
+#-----------------------------------------------------------------------------
+now = datetime.utcnow() # Now time in UTC format 
+past = now - timedelta(minutes=60) # Minus 60 minutes
+
+
+def ec2_metrics_amm():
+
+    cw_client = boto3.client('cloudwatch')
+    
+    metrics = {}
+    metrics_holder = []
+
+    response = ec2_client.describe_instances()
+    for reservation in response["Reservations"]:
+        for instance in reservation["Instances"]:
+            #print(instance["InstanceId"])
+
+            EC2CPUUtilization = cw_client.get_metric_statistics(
+                Namespace = 'AWS/EC2',
+                MetricName = 'CPUUtilization',
+                StartTime=datetime.utcnow() - timedelta(days=2) ,
+                EndTime=datetime.utcnow(),
+                Period = 86400,
+                Statistics=['Maximum','Minimum','Average'],
+                Dimensions = [
+                    {
+                        'Name': 'InstanceId',
+                        'Value': instance['InstanceId']
+                    }   
+                ]        
+            ) 
+          #  return print(EC2CPUUtilization)
+                
+            datapoints = EC2CPUUtilization['Datapoints']     # CPU Utilization results                
+            sorted_datapoint = sorted(datapoints, key=itemgetter('Timestamp'))
+
+            for i in range(len(sorted_datapoint)):
+                sorted_datapoint[i]['sort_by'] = i
+
+            last_datapoint = sorted_datapoint[-1]  # Last result
+            last_max_utilization = last_datapoint['Maximum'] # Last utilization
+            last_min_utilization = last_datapoint['Minimum'] # Last utilization
+            last_avg_utilization = last_datapoint['Average'] # Last utilization
+                                            
+                                            
+            # avg_load = round((last_avg_utilization / 100.0) 3)  # Last utilization in %
+            # timestamp = str(last_datapoint['Timestamp'])         # Last utilization timestamp
+
+            # max_load = round((last_max_utilization / 100.0), 3)
+            # min_load = round((last_min_utilization / 100.0), 3)
+
+            avg_load = round((last_avg_utilization), 3)  # Last utilization in %
+            timestamp = str(last_datapoint['Timestamp'])         # Last utilization timestamp
+
+            max_load = round((last_max_utilization), 3)
+            min_load = round((last_min_utilization), 3)
+            #print("{0} load at {1}".format(load, timestamp))
+
+            # result['selected_datapoints'] = datapoints
+            # result['last_avg'] = last_avg_utilization
+            # result['last_min'] = last_min_utilization
+            # result['last_max'] = last_max_utilization
+            # result['avg_load'] = avg_load
+            # result['last_time'] = timestamp                
+            # instance['mainState'] = result
+            # instance_holder.append(instance)
+            metrics['last_avg'] = avg_load
+            metrics['last_max'] = max_load
+            metrics['last_min'] = min_load
+            
+            metrics_holder.append(metrics)
+            # response["header"][""]
+            return metrics_holder
+            
+#------------------------------------------------------------------------------
+# [{'AmiLaunchIndex': 0, 'ImageId': 'ami-064d05b4fe8515623', 'InstanceId': 'i-043567371c05991f9', 'InstanceType': 't1.micro', 'KeyName': 'ec2-key-pair', 'LaunchTime': datetime.datetime(2022, 12, 22, 16, 25, 14, tzinfo=tzutc()), 'Monitoring': {'State': 'disabled'}, 'Placement': {'AvailabilityZone': 'us-east-1b', 'GroupName': '', 'Tenancy': 'default'}, 'Platform': 'windows', 'PrivateDnsName': 'ip-172-31-21-12.ec2.internal', 'PrivateIpAddress': '172.31.21.12', 'ProductCodes': [], 'PublicDnsName': '', 'State': {'Code': 80, 'Name': 'stopped'}, 'StateTransitionReason': 'User initiated (2022-12-22 23:22:21 GMT)', 'SubnetId': 'subnet-0496468f4b393d676', 'VpcId': 'vpc-0e8dc4407141eb883', 'Architecture': 'x86_64', 'BlockDeviceMappings': [{'DeviceName': '/dev/sda1', 'Ebs': {'AttachTime': datetime.datetime(2022, 12, 4, 17, 49, 27, tzinfo=tzutc()), 'DeleteOnTermination': True, 'Status': 'attached', 'VolumeId': 'vol-0102a32c11923a8c1'}}], 'ClientToken': '6d251cb0-46ab-4e32-8b29-fa85f584488f', 'EbsOptimized': False, 'EnaSupport': True, 'Hypervisor': 'xen', 'NetworkInterfaces': [{'Attachment': {'AttachTime': datetime.datetime(2022, 12, 4, 17, 49, 26, tzinfo=tzutc()), 'AttachmentId': 'eni-attach-0b973f025345198c9', 'DeleteOnTermination': True, 'DeviceIndex': 0, 'Status': 'attached', 'NetworkCardIndex': 0}, 'Description': '', 'Groups': [{'GroupName': 'launch-wizard-2', 'GroupId': 'sg-08d4f8ebbd3bf8b10'}], 'Ipv6Addresses': [], 'MacAddress': '0a:fd:f4:be:f4:21', 'NetworkInterfaceId': 'eni-05fd1aeb96fc3be7e', 'OwnerId': '653628891621', 'PrivateDnsName': 'ip-172-31-21-12.ec2.internal', 'PrivateIpAddress': '172.31.21.12', 'PrivateIpAddresses': [{'Primary': True, 'PrivateDnsName': 'ip-172-31-21-12.ec2.internal', 'PrivateIpAddress': '172.31.21.12'}], 'SourceDestCheck': True, 'Status': 'in-use', 'SubnetId': 'subnet-0496468f4b393d676', 'VpcId': 'vpc-0e8dc4407141eb883', 'InterfaceType': 'interface'}], 'RootDeviceName': '/dev/sda1', 'RootDeviceType': 'ebs', 'SecurityGroups': [{'GroupName': 'launch-wizard-2', 'GroupId': 'sg-08d4f8ebbd3bf8b10'}], 'SourceDestCheck': True, 'StateReason': {'Code': 'Client.UserInitiatedShutdown', 'Message': 'Client.UserInitiatedShutdown: User initiated shutdown'}, 'Tags': [{'Key': 'Name', 'Value': 'test-instance2'}], 'VirtualizationType': 'hvm', 'CpuOptions': {'CoreCount': 1, 'ThreadsPerCore': 1}, 'CapacityReservationSpecification': {'CapacityReservationPreference': 'open'}, 'HibernationOptions': {'Configured': False}, 'MetadataOptions': {'State': 'applied', 'HttpTokens': 'optional', 'HttpPutResponseHopLimit': 1, 'HttpEndpoint': 'enabled', 'HttpProtocolIpv6': 'disabled', 'InstanceMetadataTags': 'disabled'}, 'EnclaveOptions': {'Enabled': False}, 'PlatformDetails': 'Windows', 'UsageOperation': 'RunInstances:0002', 'UsageOperationUpdateTime': datetime.datetime(2022, 12, 4, 17, 49, 26, tzinfo=tzutc()), 'PrivateDnsNameOptions': {'HostnameType': 'ip-name', 'EnableResourceNameDnsARecord': True, 'EnableResourceNameDnsAAAARecord': False}, 'MaintenanceOptions': {'AutoRecovery': 'default'}, 'mainState': {'selected_datapoints': [{'Timestamp': datetime.datetime(2022, 12, 22, 18, 22, tzinfo=tzutc()), 'Average': 0.7333448677986331, 'Minimum': 0.645161290322544, 'Maximum': 1.03448275862073, 'Unit': 'Percent', 'sort_by': 1}, {'Timestamp': datetime.datetime(2022, 12, 21, 18, 22, tzinfo=tzutc()), 'Average': 3.5355675644290567, 'Minimum': 0.645161290322544, 'Maximum': 51.8032786885246, 'Unit': 'Percent', 'sort_by': 0}], 'last_avg': 0.7333448677986331, 'last_min': 0.645161290322544, 'last_max': 1.03448275862073, 'avg_load': 0.007, 'last_time': '2022-12-22 18:22:00+00:00'}}]
 
 
 #-----------------------------------------------------------------------------
